d78f9eb718967b265a46cdb8a903961b114fb908
Hassah <roxirose03@gmail.com>
2023-12-30 22:13:36 -0800

    Created a child component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-
+import Booklist from './components/Booklist'
 function App(){
   return(
     <>
     <h1>React-Book-Store</h1>
     <p>Here you can explore  various stories and books</p>
+    <Booklist/>
     </>
-  )
+  );
 }
 
 
---
+++ src/components/Booklist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Booklist(){
+    return (
+     <div>
+        <ul>
+            <li>My Book - 1</li>
+            <li>My Book - 2</li>
+            <li>My Book - 3</li>
+        </ul>
+     </div>
+    )
+}
