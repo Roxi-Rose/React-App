93b4d19787f5c5dce2d23999fe17e131e207c525
Hassah <roxirose03@gmail.com>
2024-01-04 18:07:00 -0800

    Managed state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import './App.css';
-import Booklist from './components/Booklist'; 
-function App(){
+import BookList from './components/Booklist'; 
+import Addbookform from './components/AddBookForm';
+function App() {
   const[bookli, setbookli]= useState(['My Book- 1', 'My Book- 2', 'My Book- 3']); 
-  return(
+
+  const handleClick = (event) => { 
+    setbookli([...bookli, event]);
+  }
+   return (
     <>
-    <h1>React-Book-Store</h1>
-    <p>Here you can explore  various stories and books</p>
-    <Booklist list={bookli}/>
-    </>
+   <h1>React-Book-Store</h1>
+   <p>Here you can explore  various stories and books</p>
+   <Addbookform form={handleClick}/>
+   <BookList list={bookli}/>
+   </>
   );
 }
 
