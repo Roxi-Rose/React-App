15debaf1f46fee8b89c7611c696f7898dddefceb
Hassah <roxirose03@gmail.com>
2024-01-04 18:22:38 -0800

    Event handling and dynamic rendering

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -4,9 +4,9 @@ export default function Booklist(props){
     return (
      <div>
         <ul>
-            <li>{props.list[0]}</li>
-            <li>{props.list[1]}</li>
-            <li>{props.list[2]}</li>
+        {props.list.map((dynamicBook, i)=> (
+            <li key={i}>{dynamicBook}</li>
+        ))}
         </ul>
      </div>
     )
